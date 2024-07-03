613cc82521874c659266925eea9ee83fc87a8fdc
Ricardo Garcia <developmentland03@gmail.com>
2024-07-03 17:04:45 -0600

    first commit

---
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderItemEntity.java
@@ -0,0 +1,34 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@IdClass(OrderItemId.class)
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderItemEntity {
+
+    @Id
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Id
+    @Column(name = "id_item", nullable = false)
+    private Integer idItem;
+
+    @Column(name = "id_pizza", nullable = false)
+    private Integer idPizza;
+
+    @Column(nullable = false, columnDefinition = "Decimal(2,1)")
+    private Double quantity;
+
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
+    private Double price;
+
+
+}
